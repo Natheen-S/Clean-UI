da3d59832d9e393d703dfee60fc4e8b751402716
Natheen-S <96934524+Natheen-S@users.noreply.github.com>
2023-01-06 00:06:01 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clean_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton== MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if(e.RightButton== MouseButtonState.Pressed)
+            {
+                Close();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtbox_name.Text!=null && txtbox_email.Text!=null && txtbox_email.Text!=null && txtbox_age.Text!=null && rdbtn_male.IsChecked==true || rdbtn_female.IsChecked==true) 
+            {
+                MessageBox.Show("All details are successfully submitted","Register",MessageBoxButton.OK,MessageBoxImage.Information);
+            }
+        }
+    }
+}
